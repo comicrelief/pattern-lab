250609f8b74e106699f91c409eda7e3e18d2c9c4
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-04-20 16:31:15 +0100

    change_path new styleguide path

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,62 +43,62 @@ module.exports = function (grunt) {
     kss: {
       options: {
         verbose: true,
-        css: '../css/base/all.css',
+        css: '/css/base/core.css',
         builder: 'node_modules/kss/builder/twig'
       },
       all: {
         options: {
           verbose: true,
           title: 'Comic Relief PatternLab',
-          css: '../css/base/all.css'
+          css: '/css/themes/all/all.css'
         },
         src: ['sass/themes/all'],
-        dest: 'styleguide'
+        dest: ''
       },
       cr17: {
         options: {
           verbose: true,
           title: 'Comic Relief PatternLab',
-          css: '../css/themes/cr/2017/cr17.css'
+          css: '/css/themes/cr/2017/cr17.css'
         },
         src: ['sass/base', 'sass/themes/cr/2017'],
-        dest: 'styleguide/cr'
+        dest: 'cr'
       },
       rnd17: {
         options: {
           verbose: true,
           title: 'Red Nose Day PatternLab',
-          css: '../css/themes/rnd17.css',
+          css: '/css/themes/rnd/2017/rnd17.css',
         },
         src: ['sass/base/core', 'sass/themes/rnd/2017'],
-        dest: 'styleguide/rnd'
+        dest: 'rnd'
       },
       sr18: {
         options: {
           verbose: true,
           title: 'Sport Relief PatternLab',
-          css: '../css/themes/sr/2018/sr18.css',
+          css: '/css/themes/sr/2018/sr18.css',
         },
         src: ['sass/base/core', 'sass/themes/sr/2018'],
-        dest: 'styleguide/sr'
+        dest: 'sr'
       },
       payin: {
         options: {
           verbose: true,
           title: 'Payin Online PatternLab',
-          css: '../css/themes/payin/payin.css',
+          css: '/css/themes/payin/payin.css',
         },
         src: ['sass/base/core', 'sass/themes/payin'],
-        dest: 'styleguide/payin'
+        dest: 'payin'
       },
       frost: {
         options: {
           verbose: true,
           title: 'Frost PatternLab',
-          css: '../css/themes/frost/frost.css',
+          css: '/css/themes/frost/frost.css',
         },
         src: ['sass/base/core', 'sass/themes/frost'],
-        dest: 'styleguide/frost'
+        dest: 'frost'
       }
     }
   });
