139e1074d676b554fd708964c4ac73d26a2c07da
Ayan Ozturk <a.ozturk@comicrelief.com>
2017-04-28 12:39:07 +0100

    get reference from production

--- tests/visual/html_report/config.js
+++ tests/visual/html_report/config.js
@@ -4,12 +4,12 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_0_kss-header_0_mobile.png",
-        "test": "../result/20170428-115545/visual_Homepage_0_kss-header_0_mobile.png",
+        "test": "../result/20170428-123758/visual_Homepage_0_kss-header_0_mobile.png",
         "selector": ".kss-header",
         "fileName": "visual_Homepage_0_kss-header_0_mobile.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
@@ -25,20 +25,19 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_1_kssref-0_0_mobile.png",
-        "test": "../result/20170428-115545/visual_Homepage_1_kssref-0_0_mobile.png",
+        "test": "../result/20170428-123758/visual_Homepage_1_kssref-0_0_mobile.png",
         "selector": "#kssref-0",
         "fileName": "visual_Homepage_1_kssref-0_0_mobile.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "10.60",
-          "analysisTime": 21,
+          "misMatchPercentage": "0.00",
           "getDiffImage": null
         }
       },
@@ -47,12 +46,12 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_0_kss-header_1_tablet.png",
-        "test": "../result/20170428-115545/visual_Homepage_0_kss-header_1_tablet.png",
+        "test": "../result/20170428-123758/visual_Homepage_0_kss-header_1_tablet.png",
         "selector": ".kss-header",
         "fileName": "visual_Homepage_0_kss-header_1_tablet.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
@@ -68,20 +67,19 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_1_kssref-0_1_tablet.png",
-        "test": "../result/20170428-115545/visual_Homepage_1_kssref-0_1_tablet.png",
+        "test": "../result/20170428-123758/visual_Homepage_1_kssref-0_1_tablet.png",
         "selector": "#kssref-0",
         "fileName": "visual_Homepage_1_kssref-0_1_tablet.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "5.45",
-          "analysisTime": 40,
+          "misMatchPercentage": "0.00",
           "getDiffImage": null
         }
       },
@@ -90,12 +88,12 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_0_kss-header_2_desktop.png",
-        "test": "../result/20170428-115545/visual_Homepage_0_kss-header_2_desktop.png",
+        "test": "../result/20170428-123758/visual_Homepage_0_kss-header_2_desktop.png",
         "selector": ".kss-header",
         "fileName": "visual_Homepage_0_kss-header_2_desktop.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
@@ -111,20 +109,19 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_1_kssref-0_2_desktop.png",
-        "test": "../result/20170428-115545/visual_Homepage_1_kssref-0_2_desktop.png",
+        "test": "../result/20170428-123758/visual_Homepage_1_kssref-0_2_desktop.png",
         "selector": "#kssref-0",
         "fileName": "visual_Homepage_1_kssref-0_2_desktop.png",
         "label": "Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 11,
+        "misMatchThreshold": 1,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "5.15",
-          "analysisTime": 28,
+          "misMatchPercentage": "0.00",
           "getDiffImage": null
         }
       },
