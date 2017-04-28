5350c6a474095f92adc73f6a41387da2126b34ab
Jessie Wang <j.wang@comicrelief.com>
2017-04-28 10:46:01 +0100

    PL-7 update tests due to font issue

--- tests/visual/html_report/config.js
+++ tests/visual/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../reference/visual_Homepage_0_kss-header_0_mobile.png",
-        "test": "../result/20170427-130731/visual_Homepage_0_kss-header_0_mobile.png",
+        "test": "../result/20170428-104345/visual_Homepage_0_kss-header_0_mobile.png",
         "selector": ".kss-header",
         "fileName": "visual_Homepage_0_kss-header_0_mobile.png",
         "label": "Homepage",
@@ -24,10 +24,32 @@ report({
     },
     {
       "pair": {
-        "reference": "../reference/visual_Homepage_1_kss-nav_0_mobile.png",
-        "test": "../result/20170427-130731/visual_Homepage_1_kss-nav_0_mobile.png",
-        "selector": ".kss-nav",
-        "fileName": "visual_Homepage_1_kss-nav_0_mobile.png",
+        "reference": "../reference/visual_Homepage_1_kssref-0_0_mobile.png",
+        "test": "../result/20170428-104345/visual_Homepage_1_kssref-0_0_mobile.png",
+        "selector": "#kssref-0",
+        "fileName": "visual_Homepage_1_kssref-0_0_mobile.png",
+        "label": "Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -18
+          },
+          "misMatchPercentage": "22.55",
+          "analysisTime": 26,
+          "getDiffImage": null
+        },
+        "diffImage": "../result/20170428-104345/failed_diff_visual_Homepage_1_kssref-0_0_mobile.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../reference/visual_Homepage_0_kss-header_1_tablet.png",
+        "test": "../result/20170428-104345/visual_Homepage_0_kss-header_1_tablet.png",
+        "selector": ".kss-header",
+        "fileName": "visual_Homepage_0_kss-header_1_tablet.png",
         "label": "Homepage",
         "misMatchThreshold": 0.1,
         "diff": {
@@ -37,7 +59,7 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.00",
-          "analysisTime": 14,
+          "analysisTime": 7,
           "getDiffImage": null
         }
       },
@@ -45,10 +67,32 @@ report({
     },
     {
       "pair": {
-        "reference": "../reference/visual_Homepage_0_kss-header_1_tablet.png",
-        "test": "../result/20170427-130731/visual_Homepage_0_kss-header_1_tablet.png",
+        "reference": "../reference/visual_Homepage_1_kssref-0_1_tablet.png",
+        "test": "../result/20170428-104345/visual_Homepage_1_kssref-0_1_tablet.png",
+        "selector": "#kssref-0",
+        "fileName": "visual_Homepage_1_kssref-0_1_tablet.png",
+        "label": "Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -54
+          },
+          "misMatchPercentage": "10.27",
+          "analysisTime": 55,
+          "getDiffImage": null
+        },
+        "diffImage": "../result/20170428-104345/failed_diff_visual_Homepage_1_kssref-0_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../reference/visual_Homepage_0_kss-header_2_desktop.png",
+        "test": "../result/20170428-104345/visual_Homepage_0_kss-header_2_desktop.png",
         "selector": ".kss-header",
-        "fileName": "visual_Homepage_0_kss-header_1_tablet.png",
+        "fileName": "visual_Homepage_0_kss-header_2_desktop.png",
         "label": "Homepage",
         "misMatchThreshold": 0.1,
         "diff": {
@@ -66,66 +110,25 @@ report({
     },
     {
       "pair": {
-        "reference": "../reference/visual_Homepage_1_kss-nav_1_tablet.png",
-        "test": "../result/20170427-130731/visual_Homepage_1_kss-nav_1_tablet.png",
-        "selector": ".kss-nav",
-        "fileName": "visual_Homepage_1_kss-nav_1_tablet.png",
+        "reference": "../reference/visual_Homepage_1_kssref-0_2_desktop.png",
+        "test": "../result/20170428-104345/visual_Homepage_1_kssref-0_2_desktop.png",
+        "selector": "#kssref-0",
+        "fileName": "visual_Homepage_1_kssref-0_2_desktop.png",
         "label": "Homepage",
         "misMatchThreshold": 0.1,
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": -30
           },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 26,
+          "misMatchPercentage": "10.37",
+          "analysisTime": 62,
           "getDiffImage": null
-        }
+        },
+        "diffImage": "../result/20170428-104345/failed_diff_visual_Homepage_1_kssref-0_2_desktop.png"
       },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../reference/visual_Homepage_0_kss-header_2_desktop.png",
-        "test": "../result/20170427-130731/visual_Homepage_0_kss-header_2_desktop.png",
-        "selector": ".kss-header",
-        "fileName": "visual_Homepage_0_kss-header_2_desktop.png",
-        "label": "Homepage",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 9,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
-    },
-    {
-      "pair": {
-        "reference": "../reference/visual_Homepage_1_kss-nav_2_desktop.png",
-        "test": "../result/20170427-130731/visual_Homepage_1_kss-nav_2_desktop.png",
-        "selector": ".kss-nav",
-        "fileName": "visual_Homepage_1_kss-nav_2_desktop.png",
-        "label": "Homepage",
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": true,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 0
-          },
-          "misMatchPercentage": "0.00",
-          "analysisTime": 18,
-          "getDiffImage": null
-        }
-      },
-      "status": "pass"
+      "status": "fail"
     }
   ]
 });
