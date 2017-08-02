317c147689b2489f8fe13b5fe5aeedb2aa442d19
Gustavo Liedke <contact@gustavoliedke.com>
2017-08-02 14:59:28 +0100

    102 - update backstop js

--- backstopConfig.js
+++ backstopConfig.js
@@ -3,16 +3,19 @@ const pages = require('./testUrls.json').urls;
 // Do not modify the below
 
 // Use environment variables to pass in our base and ref URLs
-const baseUrl = process.env.BACKSTOP_BASE_URL;
-const refUrl = process.env.BACKSTOP_BASE_REF_URL;
+// const baseUrl = process.env.BACKSTOP_BASE_URL;
+// const refUrl = process.env.BACKSTOP_BASE_REF_URL;
+
+const baseUrl = "dist/";
+const refUrl = "http://cr-pattern-lab.netlify.com/";
 
 let scenarios = [];
 
 pages.forEach(function(page) {
   scenarios.push({
     "label": page,
-    "url": "dist/" + page,
-    "referenceUrl": "http://cr-pattern-lab.netlify.com/" + page,
+    "url": baseUrl + page,
+    "referenceUrl": refUrl + page,
     "removeSelectors": [],
     "selectorExpansion": true,
     "selectors": [
