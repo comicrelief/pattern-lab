2937e393851d42cb4a98bd200abc82f3a597b74e
Ayan Ozturk <a.ozturk@comicrelief.com>
2017-09-07 10:16:12 +0100

    use working combination of node and npm and uninstall fs

--- ci/ci_visual_regression.sh
+++ ci/ci_visual_regression.sh
@@ -5,6 +5,8 @@ set -e
 [ -f ~/.bashrc ] && . ~/.bashrc
 [ -d pattern-lab-build ] && cd pattern-lab-build
 
+# uninstalling js to use built in fs module as suggested in:
+# https://stackoverflow.com/questions/39641513/cant-find-module-fs-when-running-casperjs-on-js-file
 npm uninstall fs
 npm set progress=false
 npm run backstop-reference
