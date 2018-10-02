f7a580f7389bbd392c13f83fe846f110aefc42f6
Andy E Phipps <a.phipps@comicrelief.com>
2018-10-02 14:42:28 +0100

    JS and style tweaks, post-design review

--- sass/themes/cr/2017/components/navigation/js/expanded-footer.js
+++ sass/themes/cr/2017/components/navigation/js/expanded-footer.js
@@ -1,6 +1,8 @@
 (function ($) {
 
-  $('.footer--expanded .menu.menu--level-0:not(.menu--level-1) > li.menu-item > a').click(function(e){
+  var $menuButton = $('.footer--expanded .menu.menu--level-0:not(.menu--level-1) > li.menu-item > a');
+
+  $menuButton.click(function(e){
     e.preventDefault();
     $(this).toggleClass('js-show');
   })
