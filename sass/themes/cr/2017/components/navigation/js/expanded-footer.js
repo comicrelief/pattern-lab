1447eddcbddba215184420ca848470a6f505f20e
Andy E Phipps <a.phipps@comicrelief.com>
2018-10-04 13:20:52 +0100

    JS updates, handles click and blur/focus simulatenously

--- sass/themes/cr/2017/components/navigation/js/expanded-footer.js
+++ sass/themes/cr/2017/components/navigation/js/expanded-footer.js
@@ -1,74 +1,27 @@
-/*(function ($) {
-
-  var $menuButton = $('.footer--expanded .menu.menu--level-0:not(.menu--level-1) > li.menu-item > a');
-
-  $menuButton.click(function(e){
-    e.preventDefault();
-    $(this).toggleClass('js-show');
-  })
-
-})(jQuery);*/
-
-
-
 (function ($) {
 
-  var animationSpeed = 250;
-  var totalItems = 0;
+  var $context = $('.navigation.menu--expanded-footer');
 
-  $('#block-expandedfooter').addClass("crExpandedFooter-processed");
+  $context.addClass("crExpandedFooter-processed");
 
   setUpFooter();
   
   function setUpFooter() {
-
-    $('#block-expandedfooter .menu-item a').wrapInner('<span class="menu-item__text"></span>');
-
-    toggleSubMenu();
+    $('.menu-item a', $context).wrapInner('<span class="menu-item__text"></span>');
+    stopClick();
     focusState();
   }
 
-
-  function toggleSubMenu() {
-
-    $context = $('#block-expandedfooter');
-
+  function stopClick() {
     $('li.menu-item--expanded > a', $context).on('click', function (e) {
-
       e.preventDefault();
-
-      // Only run this code if we're not on LG
-      if ( $('span.lg-breakpoint').is(":not(:visible)")) {
-        
-        console.log("not LG");
-
-        $this = $(this);
-        $listItem = $this.parent('li.menu-item--expanded');
-        $listItemParents = $listItem.parents('li.item-open');
-
-        // Remove any 'item-open' classes and add class to clicked item
-        $('li.item-open', $context).not($listItem).not($listItemParents).attr('aria-expanded', 'false').removeClass('item-open');
-
-        // Remove expanded attribute from all menu items
-        $('li.menu-item--expanded > a', $context).attr('aria-expanded', 'false');
-
-        // Add active class
-        $listItem.addClass('item-open');
-
-        // Add expanded state
-        $this.attr('aria-expanded', 'true');
-      }
-    });    
+    });
   }
 
   function focusState() {
 
-    // Set our context for non-feature nav
-    $context = $('#block-expandedfooter');
-
     // To store the currently focused/blurred anchor
     var $thisAnchor = null;
-    var focusState = false;
 
     // Cache our selectors
     var $subAnchor = $('li.menu-item--expanded > a ~ ul li a', $context);
@@ -76,42 +29,45 @@
     var $parentLi = $('li.menu-item--expanded', $context);
 
     $('a', $context).on('focus blur', function(e) {
-      
-      // Cache the anchor being focused/blurred
-      $thisAnchor = $(this);
 
-      /* FOCUS event */
-      if ( e.type == "focus" ? true : false ) {
-
-        // Focussed on a non-nav anchor? Remove active states
-        if (!($thisAnchor.is($('a', $context)))) {
-          $parentLi.removeClass("focused item-open").children('a').attr('aria-expanded', 'false'); 
+      if ( $('span.lg-breakpoint', $context).is(":not(:visible)")) {
+        
+        console.log('focusState', e);
+        
+        // Cache the anchor being focused/blurred
+        $thisAnchor = $(this);
+  
+        /* FOCUS event */
+        if ( e.type == "focus" ? true : false ) {
+  
+          // Focussed on a non-nav anchor? Remove active states
+          if (!($thisAnchor.is($('a', $context)))) {
+            $parentLi.removeClass("focused item-open").children('a').attr('aria-expanded', 'false'); 
+          }
+  
+          // If we're focussing on a nav item anchor, add focus class to the parent li, so we can affect all subnav styling
+          else {
+            if ($thisAnchor.is($parentAnchor)) {
+              // Unfocus all other focussed parent items
+              $parentLi.removeClass("focused item-open");
+              $(this).closest( $parentLi ).addClass("focused item-open").children('a').attr('aria-expanded', 'true');
+  
+            }
+            // Else, check its not a subitem, then remove focus class from all nav item
+            else if (!($thisAnchor.is($subAnchor))) {
+              $parentLi.removeClass("focused item-open").children('a').attr('aria-expanded', 'false'); ; 
+            }
+          }
         }
-
-        // If we're focussing on a nav item anchor, add focus class to the parent li, so we can affect all subnav styling
+  
+        /* BLUR event */
         else {
-          if ($thisAnchor.is($parentAnchor)) {
-            // Unfocus all other focussed parent items
-            $parentLi.removeClass("focused item-open");
-            $(this).closest( $parentLi ).addClass("focused item-open").children('a').attr('aria-expanded', 'true');
-
+          // If we're blurring away from the last-child subnav item, remove our overall focus class from the menu
+          if ($thisAnchor.is($subAnchor)) {
+            $thisAnchor.parent('li:last-child').closest('li.focused').removeClass("focused");
           }
-          // Else, check its not a subitem, then remove focus class from all nav item
-          else if (!($thisAnchor.is($subAnchor))) {
-            $parentLi.removeClass("focused item-open").children('a').attr('aria-expanded', 'false'); ; 
-          }
-        }
-      }
-
-      /* BLUR event */
-      else {
-        // If we're blurring away from the last-child subnav item, remove our overall focus class from the menu
-        if ($thisAnchor.is($subAnchor)) {
-          $thisAnchor.parent('li:last-child').closest('li.focused').removeClass("focused");
         }
       }
     });
   }
-
-
 })(jQuery);
