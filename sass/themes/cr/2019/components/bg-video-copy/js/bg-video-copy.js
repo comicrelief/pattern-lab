7b3315d5f3be54da78794db26003141eeb477d9d
Andy E Phipps <a.phipps@comicrelief.com>
2019-02-08 16:55:10 +0000

    Set up image and ios functionality

--- sass/themes/cr/2019/components/bg-video-copy/js/bg-video-copy.js
+++ sass/themes/cr/2019/components/bg-video-copy/js/bg-video-copy.js
@@ -5,26 +5,40 @@
 
   $(document).ready(function() {
 
+    var isIOS = navigator.userAgent.match(/iPhone|iPad|iPod/i);
+
+    // If we get an iOS match (iOS not supporting video in this way), just stick with the image
+    if (isIOS !== null) { return; }
+
+    // Otherwise, do cool video stuff
     $('.paragraph--background-video-copy').each(function(index) {
 
       $this = $(this);
+      $thisVideo = $this.find('video');
 
       // Dynamically create an ID and add it to the video for triggering purposes
       thisID = 'background-video-copy__video-' + index;
-      $this.find('video').attr('id', thisID);
+
+      $thisVideo.attr('id', thisID);
 
       // Store a ref to this video and its offset bottom position
       allVideos[index] = {
         top: $this.offset().top,
         id: thisID
       };
+
+      // Then grab our 'only request the file when we need it' 'src' data-attr, and use it for real
+      $thisSource = $thisVideo.find('source');
+      $thisSource.attr('src', $thisSource.data('src'));
+      $thisVideo.addClass('js-loaded').load();
     });
 
-    // Only attach the handler if we've got vidz
+    // Only attach the handler if we've got vids in the current page
     if (allVideos.length) {
       handleScroll();
     }
   });
+
 
   function handleScroll() {
     // Won't recalculate this *every* scroll; resizes be damned
