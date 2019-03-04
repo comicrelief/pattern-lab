612780081b748f1825cccb7d61680cfb76940520
louiaR <louitiani@gmail.com>
2019-03-04 11:38:06 +0000

    Change currency

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -1,32 +1,48 @@
 // Add active class to amount selected
 
-var paragraphs = ".paragraph--membership-signup";
+$(document).ready(function() {
+	var paragraphs = ".paragraph--membership-signup";
 
-$(paragraphs).each(function(i) {
-        var x = $(this).css('backgroundColor');
-        if(x){
-                $(this).append("<style> " + ".img-shadow" + ":before {color:" + x + "}" + "</style>" );
-        }
+	$(paragraphs).each(function(i) {
+		var x = $(this).css("backgroundColor");
+		if (x) {
+			$(this).append(
+				"<style> " + ".img-shadow" + ":before {color:" + x + "}" + "</style>"
+			);
+		}
+	});
 
-});
+	$(paragraphs).each(function(i) {
+		$(this)
+			.find(".select-amount-btn")
+			.click(function(e) {
+				$(this)
+					.parent()
+					.find(".select-amount-btn")
+					.removeClass("active");
+				// Add selected amount to input field
+				$(this).addClass("active");
+				var amount = $(this).text();
+				$(this)
+					.parent()
+					.parent()
+					.find(" input[name='membership_amount']")
+					.val(parseInt(amount.replace(/\D/g, ""), 10));
+				e.preventDefault();
+                        });
 
-
-$(paragraphs).each(function(i) {
-	$(this)
-		.find(".select-amount-btn")
-		.click(function(e) {
-			$(this)
-				.parent()
-				.find(".select-amount-btn")
-                                .removeClass("active");
-                        // Add selected amount to input field
-			$(this).addClass("active");
-			var amount = $(this).text();
-			$(this)
-				.parent()
-				.parent()
-				.find(" input[name='membership_amount']")
-				.val(parseInt(amount.replace(/\D/g, ""), 10));
-			e.preventDefault();
-		});
+		// Change currency
+		$(this)
+			.find("select")
+			.change(function() {
+				var currency = $(this)
+					.find("option:selected")
+					.data("currency");
+				$(this)
+					.parent()
+					.parent()
+					.find("#js-currency-label")
+					.text(currency);
+			});
+	});
 });
