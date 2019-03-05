bb4880af8369adc3dec263effafef09d123591e2
louiaR <louitiani@gmail.com>
2019-03-05 17:57:50 +0000

    refactoring

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -1,42 +1,43 @@
-// Add active class to amount selected
 membershipSignup();
 function membershipSignup() {
 	var url = "https://comicrelief.com/donate"
-
 	$(document).ready(function() {
 		var paragraphs = ".paragraph--membership-signup";
+
 		$(paragraphs).each(function(i) {
-			var x = $(this).css("backgroundColor");
-			if (x) {
-			    $(this).append("<style> " + ".img-shadow" + ":before {color:" + x + "}" + "</style>");
+			var colour = $(this).css("backgroundColor");
+
+			if (colour) {
+			    $(this).append("<style> " + ".img-shadow" + ":before {color:" + colour + "}" + "</style>");
 			}
 
 			// handle money buy selection
 			$(this)
 				.find(".select-amount-btn")
+				// Add and remove active class
 				.click(function(e) {
-					$(this).parent().find(".select-amount-btn").removeClass("active");
-					// Add selected money buy  amount to input field
-					$(this).addClass("active");
-					var amount = $(this).text();
-					$(this).parent().parent().find(" input[name='membership_amount']").val(parseInt(amount.replace(/\D/g, ""), 10));
 					e.preventDefault();
+					$(this).parent().find(".select-amount-btn").removeClass("active");
+					$(this).addClass("active");
+
+					var amount = $(this).text();
+
+					// Add selected money buy  amount to input field
+					$(this).parents(".membership-signup__form-money").find(" input[name='membership_amount']").val(parseInt(amount.replace(/\D/g, ""), 10));
 				});
 
 			// Handle change of currency
 			$(this)
-				.find("select")
+				.find(".form__field.form__field--currency")
 				.change(function() {
 					var currency = $(this).find("option:selected").data("currency");
-					$(this).parent().parent().parent().find(".membership__currency-label").text(currency);
+					$(this).parents(".membership-signup__form-money").find(".membership__currency-label").text(currency);
 				});
 
 			// Submit data
 			function nextStepHandler(e, currency, amount, givingType) {
                 e.preventDefault();
                 if (amount && (amount > 1 && amount <= 5000)) {
-                    var moneyDonated = currency + amount;
-					console.log(moneyDonated, givingType);
 					window.location.href = url + "?amount=" + amount + "&currency=" + currency + "&givingType=" + givingType
                 }
 			}
@@ -61,13 +62,14 @@ function membershipSignup() {
 			$(this)
 				.find(".btn--membership-blue")
 				.click(function(e) {
-                    e.preventDefault();
+					e.preventDefault();
+					var sibling = $(this).siblings()
                     // Get amount
-                    var amount = $(this).siblings().find("input[name='membership_amount']").val();
+                    var amount = sibling.find("input[name='membership_amount']").val();
                     // Get currency
-					var currency = $(this).siblings().find("#js-currency-label").text();
+					var currency = sibling.find("#js-currency-label").text();
 					// Giving type
-					var givingType = $(this).closest('form').data('giving-type');
+					var givingType = $(this).closest('form.membership-signup__form-money').data('giving-type');
 					// Send data
 				    nextStepHandler(e, currency, amount, givingType);
 				});
