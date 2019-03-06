3bd2435f9315c05e160c4a4f7cc45a0d99ce5624
Andy E Phipps <a.phipps@comicrelief.com>
2019-03-06 12:04:47 +0000

    JS refactoring

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -1,79 +1,94 @@
-membershipSignup();
-function membershipSignup() {
-	var url = "https://comicrelief.com/donate"
-	$(document).ready(function() {
-		var paragraphs = ".paragraph--membership-signup";
+(function($) {
 
-		$(paragraphs).each(function(i) {
-			var colour = $(this).css("backgroundColor");
+	$(document).ready(function() {
+
+		var url = "https://comicrelief.com/donate"
+
+		$('.paragraph--membership-signup').each(function(i) {
+
+			var $thisParagraph = $(this);
+
+			var thisID = 'paragraph--membership-signup-' + i;
+
+			$thisParagraph.attr('id', thisID);
+
+			var colour = $thisParagraph.css("backgroundColor");
 
 			if (colour) {
-			    $(this).append("<style> " + ".img-shadow" + ":before {color:" + colour + "}" + "</style>");
+				$('.img-shadow', $thisParagraph).append("<style> " + "#" + thisID + " .img-shadow" + ":before {color:" + colour + "}" + "</style>");
 			}
-
-			// handle money buy selection
-			$(this)
-				.find(".select-amount-btn")
-				// Add and remove active class
-				.click(function(e) {
-					e.preventDefault();
-					$(this).parent().find(".select-amount-btn").removeClass("active");
-					$(this).addClass("active");
-
-					var amount = $(this).text();
-
-					// Add selected money buy  amount to input field
-					$(this).parents(".membership-signup__form-money").find(" input[name='membership_amount']").val(parseInt(amount.replace(/\D/g, ""), 10));
-				});
-
-			// Handle change of currency
-			$(this)
-				.find(".form__field.form__field--currency")
-				.change(function() {
-					var currency = $(this).find("option:selected").data("currency");
-					$(this).parents(".membership-signup__form-money").find(".membership__currency-label").text(currency);
-				});
-
-			// Submit data
-			function nextStepHandler(e, currency, amount, givingType) {
-                e.preventDefault();
-                if (amount && (amount > 1 && amount <= 5000)) {
-					window.location.href = url + "?amount=" + amount + "&currency=" + currency + "&givingType=" + givingType
-                }
-			}
-			// keyboard event
-			$(this)
-				.find("input[name='membership_amount']")
-				.keypress(function(e) {
-					if (e.which == 13) {
-						e.preventDefault();
-						// Get amount
-						var amount = parseFloat($(this).val(), 10);
-						// Get currency
-						var currency = $(this).siblings(".currency-input-label").text();
-						// Giving type
-						var givingType = $(this).closest('form').data('giving-type');
-						// Send data
-                        nextStepHandler(e, currency, amount, givingType);
-					}
-				});
-
-			// button event
-			$(this)
-				.find(".btn--membership-blue")
-				.click(function(e) {
-					e.preventDefault();
-					var sibling = $(this).siblings()
-                    // Get amount
-                    var amount = sibling.find("input[name='membership_amount']").val();
-                    // Get currency
-					var currency = sibling.find("#js-currency-label").text();
-					// Giving type
-					var givingType = $(this).closest('form.membership-signup__form-money').data('giving-type');
-					// Send data
-				    nextStepHandler(e, currency, amount, givingType);
-				});
 		});
-	});
-}
 
+		/* Handle money buy selection */
+		$('.paragraph--membership-signup .select-amount-btn').click(function(e) {
+
+			e.preventDefault();
+
+			var $thisBtn = $(this);
+			$thisBtn.siblings('.select-amount-btn').removeClass("active");
+			$thisBtn.addClass("active");
+
+			var amount = $thisBtn.text();
+
+			$thisBtn
+			.parents(".membership-signup__form-money")
+			.find("input[name='membership_amount']").val(parseInt(amount.replace(/\D/g, ""), 10));
+
+		});
+
+
+		/* Handle change of currency */
+		$('.paragraph--membership-signup select').change(function() {
+			var $thisSelect = $(this);
+			var currency = $thisSelect.find("option:selected").data("currency");
+			$thisSelect.closest(".membership-signup__form-money").find(".membership__currency-label").text(currency);
+		});
+
+
+		/* Handle enter-key keyboard event */
+		$(".paragraph--membership-signup input[name='membership_amount']").keypress(function(e) {
+
+			var $thisInput = $(this);
+
+			if (e.which == 13) {
+				e.preventDefault();
+          // Get amount
+          var amount = parseFloat($thisInput.val(), 10);
+          // Get currency
+          var currency = $thisInput.siblings(".currency-input-label").text();
+          // Giving type
+          var givingType = $thisInput.closest('form').data('giving-type');
+          // Send data
+          nextStepHandler(e, currency, amount, givingType);
+        }
+      });
+
+      // button event
+      $(".paragraph--membership-signup .membership--submit").click(function(e) {
+      	e.preventDefault();
+
+      	var $thisButton = $(this);
+
+      	var siblings = $thisButton.siblings();
+
+      	var $thisForm = $thisButton.closest('form');
+
+        // Get amount
+        var amount = $thisForm.find("input[name='membership_amount']").val();
+        // Get currency
+        var currency = $thisForm.find("#js-currency-label").text();
+        // Giving type
+        var givingType = $thisForm.data('giving-type');
+        // Send data
+        nextStepHandler(e, currency, amount, givingType);
+      });
+
+      /* Submit data */
+      function nextStepHandler(e, currency, amount, givingType) {
+      	e.preventDefault();
+      	if (amount && (amount > 1 && amount <= 5000)) {
+      		window.location.href = url + "?amount=" + amount + "&currency=" + currency + "&givingType=" + givingType
+      	}
+      }
+    });
+})(jQuery);
