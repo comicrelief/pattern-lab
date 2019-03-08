2e1d0815ffa7c08ec16dd2dbeebefd994cfae126
louiaR <louitiani@gmail.com>
2019-03-08 12:42:05 +0000

    Fix overlap bugs

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -3,36 +3,27 @@
 	$(document).ready(function() {
 
 		var url = "https://comicrelief.com/donate"
-		var pattern = /^[1-9]+([,.][0-9]+)?$/;
+		var pattern = /^[0-9]+([,.][0-9]+)?$/;
 
 		$('.paragraph--membership-signup').each(function(i) {
-
 			var $thisParagraph = $(this);
-
 			var thisID = 'paragraph--membership-signup-' + i;
-
 			$thisParagraph.attr('id', thisID);
-
 			var colour = $thisParagraph.css("backgroundColor");
-
 			if (colour) {
 				$('.img-shadow', $thisParagraph).append("<style> " + "#" + thisID + " .img-shadow" + ":before {color:" + colour + "}" + "</style>");
 			}
 		});
 
+
 		/* Handle money buy selection */
 		$('.paragraph--membership-signup .select-amount-btn').click(function(e) {
-
 			e.preventDefault();
-
 			var $thisBtn = $(this);
 			$thisBtn.siblings('.select-amount-btn').removeClass("active");
 			$thisBtn.addClass("active");
-
 			var amount = $thisBtn.text();
-
-			$thisBtn
-			.parents(".membership-signup__wrapper-copy--form-money")
+			$thisBtn.parents(".membership-signup__wrapper-copy--form-money")
 			.find("input[name='membership_amount']").val(parseFloat(amount.replace(/\D/g, "")));
 
 		});
@@ -56,18 +47,22 @@
 			}
 		});
 
+
 		/* Handle enter-key keyboard event */
 		$(".paragraph--membership-signup input[name='membership_amount']").keypress(function(e) {
 			var $thisInput = $(this);
 			if (e.which == 13) {
 				e.preventDefault();
-          // Get amount
+          /* Get amount */
 					var amount = parseFloat($thisInput.val());
-          // Get currency
-          var currency = $thisInput.siblings(".currency-input-label").text();
-          // Giving type
+          /* Get currency */
+					var currency = $thisInput.closest('form').find("option:selected").val();
+          /* Giving type */
 					var givingType = $thisInput.closest('form').data('giving-type');
-					// Send data
+					/* Cart ID */
+					var cartId = $thisInput.closest('form').data('cart-id');
+					console.log(cartId)
+					/* Send data */
 					if (validateAmount(amount)) {
 						$thisInput.closest('form').find(".form-error").removeClass('visible');
 						nextStepHandler(e, currency, amount, givingType);
@@ -81,20 +76,17 @@
       // Handle pressing next button event
       $(".paragraph--membership-signup .membership--submit").click(function(e) {
       	e.preventDefault();
-
       	var $thisButton = $(this);
-
-      	// var siblings = $thisButton.siblings();
-
       	var $thisForm = $thisButton.closest('form');
-
-        // Get amount
+        /* Get amount */
         var amount = $thisForm.find("input[name='membership_amount']").val();
-        // Get currency
-        var currency = $thisForm.find("#js-currency-label").text();
-        // Giving type
+        /* Get currency */
+        var currency = $thisForm.closest('form').find("option:selected").val();
+        /* Giving type */
 				var givingType = $thisForm.data('giving-type');
-				// Send data
+				/* Cart ID */
+				var cartId = $thisForm.data('cart-id');
+				/* Send data */
 				if (validateAmount(amount)) {
 					$thisForm.find(".form-error").removeClass('visible');
 					nextStepHandler(e, currency, amount, givingType);
@@ -104,9 +96,9 @@
 
       });
 
-			/* Checking and validate amount */
+			/* Check and validate amount */
 			function validateAmount(amount) {
-				if (pattern.test(amount) && amount <= 5000) {
+				if (pattern.test(amount) && (amount >= 1 && amount <= 5000)) {
 					 return true
       	} else {
 					return false
