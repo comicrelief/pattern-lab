34aad6ecd8879794b47a6d5c3e76efbbda23d326
louiaR <louitiani@gmail.com>
2019-03-12 14:29:55 +0000

    Refactoring

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -5,27 +5,7 @@
 
 		$('.paragraph--membership-signup').each(function(i) {
 			var $thisParagraph = $(this);
-			var thisID = 'paragraph--membership-signup-' + i;
-			$thisParagraph.attr('id', thisID);
-			var $newParagraphWithId = $('#'+ thisID);
-			/* Box shadow colour */
-			var colour = $newParagraphWithId.css("backgroundColor");
-			if (colour) {
-				$('.img-shadow', $newParagraphWithId).append("<style> " + "#" + thisID + " .img-shadow" + ":before {color:" + colour + "}" + "</style>");
-			}
-			var amount = parseFloat($newParagraphWithId.find(".select-amount-btn.active").text().replace(/\D/g, ""));
-			var currency = $newParagraphWithId.find("option:selected").data("currency");
-			$newParagraphWithId.find("input[name='membership_amount']").val(amount);
-			/* Add money buy description && currency */
-			var descAmount = $newParagraphWithId.find(".money-buy--desc-amount");
-			descAmount.each(function() {
-				var $thisElement = $(this).parent();
-				$thisElement.removeClass('dis');
-				$thisElement.find(".money-buy--desc-currency").text(currency);
-				if(amount === parseFloat($(this).text())) {
-					$thisElement.addClass('dis');
-				}
-			})
+			loadPage($thisParagraph, i);
 		});
 
 
@@ -37,17 +17,13 @@
 			$thisBtn.addClass("active");
 			var amount = parseFloat($thisBtn.text().replace(/\D/g, ""));
 			var $thisBtnParent = $thisBtn.parents(".membership-signup__wrapper-copy--form-money");
-			var descAmount = $thisBtnParent.find(".money-buy--desc-amount");
+			var descriptionCopies = $thisBtnParent.find(".money-buy--desc-amount");
 			$thisBtnParent.find("input[name='membership_amount']").val(amount);
-			descAmount.each(function() {
-				$(this).parent().removeClass('dis');
-				if(amount === parseFloat($(this).text())) {
-					$(this).parent().addClass('dis');
-				}
-			})
+			currentAmount($thisBtn, amount);
+			moneyBuyDescriptionHandler(descriptionCopies, amount);
 		});
 
-		/* Handle change of currency */
+		/** Handle change of currency  and set currency of input label */
 		$('.paragraph--membership-signup select').change(function() {
 			var $thisSelect = $(this);
 			var currency = $thisSelect.find("option:selected").data("currency");
@@ -55,15 +31,18 @@
 			$thisParent.find("#js-currency-label").text(currency);
 		});
 
-		/* Watch for action / change on input */
+		/* Watch for action or change on input */
 		$(".paragraph--membership-signup input[name='membership_amount']").on("input propertychange",function(){
 			var $thisInput = $(this);
 			var amount = parseFloat($thisInput.val());
-			$(this).parents('form').find('.select-amount-btn').removeClass("active");
-			if (validateAmount(amount) || isNaN(amount)) {
-				$thisInput.closest('form').find(".form-error").removeClass('visible');
+			var $thisForm = $thisInput.parents('form');
+			$thisForm.find('.select-amount-btn').removeClass("active");
+			currentAmount($thisForm, amount);
+
+			if (validateAmount(amount) || !isNaN(amount)) {
+				$thisForm.find(".form-error").removeClass('visible');
 			} else {
-				$thisInput.closest('form').find(".form-error").addClass('visible');
+				$thisForm.find(".form-error").addClass('visible');
 			}
 		});
 
@@ -72,21 +51,9 @@
 			var $thisInput = $(this);
 			if (e.which == 13) {
 				e.preventDefault();
-          /* Get amount */
-					var amount = parseFloat($thisInput.val());
-          /* Get currency */
-					var currency = $thisInput.closest('form').find("option:selected").val();
-          /* Giving type */
-					var givingType = $thisInput.closest('form').data('giving-type');
-					/* Cart ID */
-					var cartId = $thisInput.closest('form').data('cart-id');
-					/* Send data */
-					if (validateAmount(amount)) {
-						$thisInput.closest('form').find(".form-error").removeClass('visible');
-						nextStepHandler(e, currency, amount, givingType);
-					} else {
-						$thisInput.closest('form').find(".form-error").addClass('visible');
-					}
+					var $thisForm = $thisInput.closest('form');
+					var amount = getAmount($thisForm)
+					handleDatabeforeSubmission($thisForm, amount, e);
         }
       });
 
@@ -95,37 +62,88 @@
       	e.preventDefault();
       	var $thisButton = $(this);
       	var $thisForm = $thisButton.closest('form');
-        /* Get amount */
-        var amount = $thisForm.find("input[name='membership_amount']").val();
-        /* Get currency */
-        var currency = $thisForm.closest('form').find("option:selected").val();
-        /* Giving type */
-				var givingType = $thisForm.data('giving-type');
-				/* Cart ID */
-				var cartId = $thisForm.data('cart-id');
-				/* Send data */
-				if (validateAmount(amount)) {
-					$thisForm.find(".form-error").removeClass('visible');
-					nextStepHandler(e, currency, amount, givingType);
-				} else {
-					$thisForm.find(".form-error").addClass('visible');
-				}
+				var amount = getAmount($thisForm);
+				handleDatabeforeSubmission($thisForm, amount, e);
+			});
 
-      });
 
-			/* Check and validate amount */
-			function validateAmount(amount) {
-				if (pattern.test(amount) && (amount >= 1 && amount <= 5000)) {
-					 return true
-      	} else {
-					return false
-				}
+		/**  FUNCTIONS  */
+
+
+		function loadPage($thisParagraph, i) {
+			var thisID = 'paragraph--membership-signup-' + i;
+			$thisParagraph.attr('id', thisID);
+			var $newParagraphWithId = $('#'+ thisID);
+
+			/* Set Box shadow colour */
+			var colour = $newParagraphWithId.css("backgroundColor");
+			if (colour) {
+				$('.img-shadow', $newParagraphWithId).append("<style> " + "#" + thisID + " .img-shadow" + ":before {color:" + colour + "}" + "</style>");
 			}
 
-			/* Submit data */
-      function nextStepHandler(e, currency, amount, givingType) {
-				e.preventDefault();
-      	window.location.href = url + "?amount=" + amount + "&currency=" + currency + "&givingType=" + givingType
-      }
-    });
+			var amount = parseFloat($newParagraphWithId.find(".select-amount-btn.active").text().replace(/\D/g, ""));
+			$newParagraphWithId.attr("data-current-amount", amount);
+			var currency = $newParagraphWithId.find("option:selected").data("currency");
+			/* Add money buy description && currency */
+			var descriptionCopies = $newParagraphWithId.find(".money-buy--desc-amount");
+			moneyBuyDescriptionHandler(descriptionCopies, amount);
+		}
+
+		/** Money buy description handler */
+		function moneyBuyDescriptionHandler(descriptions, amount) {
+			/* Add money buy description && currency */
+			descriptions.each(function () {
+				$(this).parent().removeClass('dis');
+				if (amount === parseFloat($(this).text())) {
+					$(this).parent().addClass('dis');
+				}
+			})
+		}
+
+		/** Set value of data current amount */
+		function currentAmount(selector, amount) {
+			if (isNaN(amount)) {
+				selector.parents(".paragraph--membership-signup").attr("data-current-amount", 0);
+			} else {
+				selector.parents(".paragraph--membership-signup").attr("data-current-amount", amount);
+			}
+		}
+
+		/** Get amount from value of data current amount  */
+		function getAmount(selector) {
+			return selector.parents(".paragraph--membership-signup").data("current-amount");
+		}
+
+		/* Handle data before submission */
+		function handleDatabeforeSubmission($thisForm, amount, event) {
+			/* Get currency */
+			var currency = $thisForm.find("option:selected").val();
+			/* Giving type */
+			var givingType = $thisForm.data('giving-type');
+			/* Cart ID */
+			var cartId = $thisForm.data('cart-id');
+			/* Send data */
+			if (validateAmount(amount)) {
+				$thisForm.find(".form-error").removeClass('visible');
+				nextStepHandler(event, currency, amount, givingType);
+			} else {
+				$thisForm.find(".form-error").addClass('visible');
+			}
+		}
+
+		/* Check and validate amount */
+		function validateAmount(amount) {
+			if (pattern.test(amount) && (amount >= 1 && amount <= 5000)) {
+				return true
+			} else {
+				return false
+			}
+		}
+
+		/* Submit data */
+		function nextStepHandler(e, currency, amount, givingType) {
+			e.preventDefault();
+			window.location.href = url + "?amount=" + amount + "&currency=" + currency + "&givingType=" + givingType
+		}
+	});
 })(jQuery);
