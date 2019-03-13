d2d9feec8c805637b887d07658a2742fe663cd01
louiaR <louitiani@gmail.com>
2019-03-13 18:26:32 +0000

    Style for monthly donation

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -14,6 +14,7 @@
 			e.preventDefault();
 			var $thisBtn = $(this);
 			var $thisForm= $thisBtn.closest('form');
+			$thisForm.find('select').css("background", "transparent");
 			$thisForm.find(".form__field--wrapper").removeClass('active-input');
 			$thisForm.find(".form-error").removeClass('show-error');
 			$thisForm.find('.select-amount-btn').removeClass("active");
@@ -27,31 +28,28 @@
 			moneyBuyDescriptionHandler(descriptionCopies, position);
 		});
 
-		// /** Handle change of currency  and set currency of input label */
-		// $('.paragraph--membership-signup select').change(function() {
-		// 	var $thisSelect = $(this);
-		// 	var currency = $thisSelect.find("option:selected").data("currency");
-		// 	var $thisParent = $thisSelect.closest(".membership-signup__wrapper-copy--form-money");
-		// 	$thisParent.find("#js-currency-label").text(currency);
-		// });
 
 		/* Watch for action or change on input */
-		$(".paragraph--membership-signup input[name='membership_amount']").on("input propertychange",function(){
+		$(".paragraph--membership-signup input[name='membership_amount']").on("input propertychange click",function(event){
 			var $thisInput = $(this);
 			var amount = parseFloat($thisInput.val());
 			var $thisForm = $thisInput.parents('form');
 			$thisForm.find(".form__field--wrapper").addClass("active-input")
 			$thisForm.find('.select-amount-btn').removeClass("active");
-			$thisForm.find('.money-buy--description').removeClass('show-copy');
-			$thisForm.find('.random-description').addClass('show-copy');
+			$thisForm.find('.money-buy--description').removeClass('show-money-buy-copy');
+			$thisForm.find('.random-description').addClass('show-money-buy-copy');
 
-			if (validateAmount(amount) && !isNaN(amount)) {
-				$thisForm.find(".form-error").removeClass('show-error');
-				currentAmount($thisForm, amount);
-			} else {
-				$thisForm.find(".form-error").addClass('show-error');
-				currentAmount($thisForm, 0);
+			// Check if user enters input
+			if (event.type === "input") {
+				if (validateAmount(amount) && !isNaN(amount)) {
+					$thisForm.find(".form-error").removeClass('show-error');
+					currentAmount($thisForm, amount);
+				} else {
+					$thisForm.find(".form-error").addClass('show-error');
+					currentAmount($thisForm, 0);
+				}
 			}
+
 		});
 
 		/* Handle enter-key keyboard event */
@@ -88,6 +86,12 @@
 				$('.img-shadow', $newParagraphWithId).append("<style> " + "#" + thisID + " .img-shadow" + ":before {color:" + colour + "}" + "</style>");
 			}
 
+			/** Check giving type before taking decision to hide select tag or not */
+			var givingType = $newParagraphWithId.find("form").data("giving-type");
+			if(givingType.toLowerCase() === "MONTHLY".toLowerCase()) {
+				$newParagraphWithId.find('.form__fieldset').addClass("hide-select-tag");
+			}
+
 			var amount = parseFloat($newParagraphWithId.find(".select-amount-btn.active").text().replace(/\D/g, ""));
 			$newParagraphWithId.attr("data-current-amount", amount);
 			var position = $newParagraphWithId.find(".select-amount-btn.active").data("position");
@@ -99,9 +103,9 @@
 		/** Money buy description handler */
 		function moneyBuyDescriptionHandler(descriptions, position) {
 			descriptions.each(function (i) {
-				$(this).removeClass('show-copy');
+				$(this).removeClass('show-money-buy-copy');
 				if (position === i + 1) {
-					$(this).addClass('show-copy');
+					$(this).addClass('show-money-buy-copy');
 				}
 			})
 		}
