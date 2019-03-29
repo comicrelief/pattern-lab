4310584dfbfe19ecadb3055b594736c7037df1cc
Andy E Phipps <a.phipps@comicrelief.com>
2019-03-29 10:38:24 +0000

    fix: siteurl param handling

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -210,7 +210,7 @@
 			var getUrl =  $('#paragraph--membership-signup-0').data("donation-url");
 			var donationLink = getUrl ? getUrl : url;
 
-			/** Affiliate value */
+			/* Affiliate value */
 			var url_string = window.location.href;
 			var url = new URL(url_string);
 			var affiliateValue = url.searchParams.get("affiliate")? url.searchParams.get("affiliate") : 'generic';
@@ -219,7 +219,7 @@
 			if (url_string.indexOf('?') > -1 ) {
 				url_string = url_string.substring(0, url_string.indexOf('?'));
 			}
-			
+
 			redirect(donationLink + "?clientOverride=" + clientId + "&amount=" + amount + "&currency=" + currency + "&givingType=" + givingType + "&cartId=" + cartId + "&affiliate=" + affiliateValue + "&siteurl=" + url_string) ;
 		}
 	});
