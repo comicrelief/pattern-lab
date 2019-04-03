2b3d9ae2c2ea7fe72a73a94509bdd6bc866006ee
louiaR <louitiani@gmail.com>
2019-04-03 12:23:13 +0100

    Refactoring

--- sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
+++ sass/themes/cr/2019/components/membership-signup/js/membership-signup.js
@@ -1,9 +1,10 @@
 (function($) {
 	$(document).ready(function() {
 		var pattern = /^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$/;
+		var moneyBuyRows = {};
+
 		/* Get  website-page url  */
 		var url_string = window.location.href;
-
 		$('.paragraph--membership-signup').each(function(i) {
 			var $thisParagraph = $(this);
 			setFormDefaults($thisParagraph, i);
@@ -39,6 +40,7 @@
 			var $thisInput = $(this);
 			var $thisForm = $thisInput.parents('form');
 			var amount = parseFloat($thisInput.val());
+			var id = $thisForm.parents(".paragraph--membership-signup").attr('id');
 
 			$thisForm.find(".form__field--wrapper").addClass("active-input")
 			$thisForm.find('.select-amount-btn').removeClass("active");
@@ -48,18 +50,15 @@
 			/** Reset current amount to zero  */
 			setCurrentDataAmount($thisInput, 0);
 
-
-			var moneyBuyArr = [];
-			moneyBuyValueArr(moneyBuyArr, $thisInput.parents('form'));
-			/* Compare money buy amount and amount provided by user */
-			if(moneyBuyArr.indexOf(amount.toString()) > -1) {
-				var index = parseFloat(moneyBuyArr.indexOf(amount.toString())) + 1;
+			/* Compare money buy value of the row  and amount provided by user */
+			if(moneyBuyRows[id].indexOf(amount.toString()) > -1) {
+				var index = parseFloat(moneyBuyRows[id].indexOf(amount.toString())) + 1;
 				$thisForm.find('.select-amount-btn.money-box--' + index ).addClass("active");
 				var descriptionCopies = $thisForm.find(".donation-copy").children();
 				moneyBuyDescriptionHandler(descriptionCopies, index);
 			}
 
-			// Check if user enters input
+			/* Check if user enters input */
 			if (event.type === "input") {
 				if (validateAmount(amount) && !isNaN(amount)) {
 					$thisForm.find(".form-error").removeClass('show-error');
@@ -124,7 +123,6 @@
 				setCurrentDataAmount($thisButton, 0);
 				handleDatabeforeSubmission($thisForm, 0, e);
 			}
-
 			$thisForm.parents('.paragraph--membership-signup').attr('data-current-amount', moneyBuySelected)
 		});
 
@@ -158,15 +156,17 @@
 			/* Add money buy description && currency */
 			var descriptionCopies = $newParagraphWithId.find(".donation-copy").children();
 
+			/* Populate moneybuyrows object with money buy value of each row */
+			moneyBuyRows[thisID] =[];
+			$newParagraphWithId.find('.select-amount-btn').each(function(i){
+				var currentMoneyBuyValue = $(this).text().replace(/\D/g, "");
+				moneyBuyRows[thisID].push(currentMoneyBuyValue)
+
+			});
+
 			/* Handle case where users are taken back to cr.com from donation */
 			if( url_string.indexOf('&amount=') > -1 && amountValue.length > 0 ) {
-				/* Create an an array that will contain money buy value  */
-				var moneyBuyArr = []
-
 				if(rowIDValue.length > 0) {
-					/*  Push money buy value to array  */
-					moneyBuyValueArr(moneyBuyArr, $("#" + rowIDValue));
-
 					$("#" + rowIDValue).find('.select-amount-btn').each(function (i) {
 						var currentMoneyBuyValue = $(this).text().replace(/\D/g, "");
 						var $thisForm = $(this).parents('form');
@@ -175,7 +175,7 @@
 							$("#" + rowIDValue).find('.select-amount-btn').removeClass('active');
 							$(this).addClass('active');
 							 moneyBuyDescriptionHandler(descriptionCopies, i + 1 );
-						} else if (moneyBuyArr.indexOf(amountValue) === -1 ) {
+						} else if (moneyBuyRows[rowIDValue].indexOf(amountValue) === -1 ) {
 							$thisForm.find("input[name='membership_amount']").val(amountValue);
 							$("#" + rowIDValue).find('.select-amount-btn').removeClass('active');
 							$thisForm.find('.form__field--wrapper').addClass("active-input");
@@ -235,7 +235,7 @@
 			}
 		}
 
-		/** Rdirect function for browser support */
+		/* Rdirect function for browser support */
 		function redirect(url) {
 			var ua = navigator.userAgent.toLowerCase(),
 				isIE = ua.indexOf('msie') !== -1,
@@ -251,14 +251,6 @@
 			else {
 				window.location.href = url;
 			}
-		}
-
-		/* Create array of money buy value */
-		function moneyBuyValueArr(array, element) {
-			element.find(".select-amount-btn").each(function() {
-				array.push($(this).text().replace(/\D/g, ""));
-			})
-			return array;
 		}
 
 		/* Submit data */
